36f520ea6b640ee500117423fa98bb3d6ad3b859
Benjamin Martens <bmartens@pm.me>
2022-08-11 18:34:41 +0200

    Start adding argument parsing

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <websocketpp/server.hpp>
+#include <websocketpp/config/asio_no_tls.hpp>
 
 int main(int argc, char const *argv[])
 {
     printf("Hello world\n");
-    /* code */
+
+    // Start server
+    websocketpp::server<websocketpp::config::asio> server;
+    server.init_asio();
+    server.listen(443);
+    server.start_accept();
+    server.run();
+
     return 0;
 }
